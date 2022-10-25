d850fb72f782fe055fd3379360ef4faf26408d2d
Tobias de Bruijn <t.debruijn@array21.dev>
2022-10-25 16:47:29 +0200

    Update Image to 0.24

--- src/render/image.rs
+++ src/render/image.rs
@@ -7,8 +7,12 @@ use image::{ImageBuffer, Luma, LumaA, Pixel as ImagePixel, Primitive, Rgb, Rgba}
 
 macro_rules! impl_pixel_for_image_pixel {
     ($p:ident<$s:ident>: $c:pat => $d:expr) => {
-        impl<$s: Primitive + 'static> Pixel for $p<$s> {
-            type Image = ImageBuffer<Self, Vec<S>>;
+        impl<$s> Pixel for $p<$s>
+        where
+            $s: Primitive + 'static,
+            Self: ImagePixel,
+        {
+            type Image = ImageBuffer<Self, Vec<<Self as ImagePixel>::Subpixel>>;
             type Canvas = (Self, Self::Image);
 
             fn default_color(color: Color) -> Self {
